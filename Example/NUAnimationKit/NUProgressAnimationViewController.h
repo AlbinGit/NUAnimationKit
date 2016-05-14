55825a468f826d51eef2ae17e3e2207b2a30d174
Victor <vgm.maraccini@gmail.com>
2016-05-14 00:29:32 -0300

    Update project to showcase pure progress-based animations

---
+++ Example/NUAnimationKit/NUProgressAnimationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NUProgressAnimationViewController.h
+//  NUAnimationKit
+//
+//  Created by Victor Gabriel Maraccini on 5/13/16.
+//  Copyright © 2016 Victor. All rights reserved.
+//
+
+#import "NUBaseExampleViewController.h"
+
+@interface NUProgressAnimationViewController : NUBaseExampleViewController
+
+@end
